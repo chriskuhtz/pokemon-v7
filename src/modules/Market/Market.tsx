1b3e5fb379b1bf722b88867d28a2580c69261b66
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-03 23:02:08 +0100

    adjust styles

--- src/modules/Market/Market.tsx
+++ src/modules/Market/Market.tsx
@@ -4,6 +4,7 @@ import { RoutesType } from '../../App';
 import { Card } from '../../uiComponents/Card/Card';
 import { Page } from '../../uiComponents/Page/Page';
 import { Stack } from '../../uiComponents/Stack/Stack';
+import { baseSize } from '../../constants/gameData';
 export const Market = ({
 	goBack,
 	navigate,
@@ -17,13 +18,13 @@ export const Market = ({
 				<Card
 					onClick={() => navigate('BUY_MARKET')}
 					content={<h4>Buy</h4>}
-					icon={<FaCartPlus size={30} />}
+					icon={<FaCartPlus size={baseSize / 2} />}
 					actionElements={[]}
 				/>
 				<Card
 					onClick={() => navigate('SELL_MARKET')}
 					content={<h4>Sell</h4>}
-					icon={<BsCashCoin size={30} />}
+					icon={<BsCashCoin size={baseSize / 2} />}
 					actionElements={[]}
 				/>
 			</Stack>
--- src/uiComponents/Stack/Stack.tsx
+++ src/uiComponents/Stack/Stack.tsx
@@ -1,13 +1,15 @@
-import { ReactNode } from 'react';
+import { CSSProperties, ReactNode } from 'react';
 
 export const Stack = ({
 	children,
 	gap,
 	mode,
+	justifyContent,
 }: {
 	children: ReactNode[];
 	gap?: number;
 	mode: 'row' | 'column';
+	justifyContent?: CSSProperties['justifyContent'];
 }): JSX.Element => {
 	return (
 		<div
@@ -15,6 +17,7 @@ export const Stack = ({
 				display: 'flex',
 				flexDirection: mode,
 				gap: `${gap ?? 0.5}rem`,
+				justifyContent,
 			}}
 		>
 			{children}
