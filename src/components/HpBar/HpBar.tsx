ac2374f299849ec7f3423fbe60ae029bfb80fbcd
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-05 20:28:55 +0100

    regulate hp bar

--- src/components/HpBar/HpBar.tsx
+++ src/components/HpBar/HpBar.tsx
@@ -6,11 +6,44 @@ export const HpBar = ({ max, damage }: { max: number; damage: number }) => {
 
 	useEffect(() => {
 		const t = setTimeout(() => {
-			const actual = max - damage;
+			const actual = Math.max(max - damage, 0);
+
 			if (current < actual) {
+				if (actual - current > 50) {
+					setCurrent(current + 37);
+					return;
+				}
+				if (actual - current > 20) {
+					setCurrent(current + 13);
+					return;
+				}
+				if (actual - current > 10) {
+					setCurrent(current + 7);
+					return;
+				}
+				if (actual - current > 5) {
+					setCurrent(current + 3);
+					return;
+				}
 				setCurrent(current + 1);
 			}
 			if (current > actual) {
+				if (current - actual > 50) {
+					setCurrent(current - 37);
+					return;
+				}
+				if (current - actual > 20) {
+					setCurrent(current - 13);
+					return;
+				}
+				if (current - actual > 10) {
+					setCurrent(current - 7);
+					return;
+				}
+				if (current - actual > 5) {
+					setCurrent(current - 3);
+					return;
+				}
 				setCurrent(current - 1);
 			}
 		}, 100);
