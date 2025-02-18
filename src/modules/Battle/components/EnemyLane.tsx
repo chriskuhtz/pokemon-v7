6b80015bcfe4f51d2598519964260171a9a8c51a
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-18 01:00:24 +0100

    add BattleInfoBoxes

--- src/modules/Battle/components/EnemyLane.tsx
+++ src/modules/Battle/components/EnemyLane.tsx
@@ -1,3 +1,4 @@
+import { HpBar } from '../../../components/HpBar/HpBar';
 import { getPokemonSprite } from '../../../functions/getPokemonSprite';
 import { BattlePokemon } from '../../../interfaces/BattlePokemon';
 
@@ -7,10 +8,39 @@ export function EnemyLane({
 	onFieldOpponents: BattlePokemon[];
 }) {
 	return (
-		<div style={{ display: 'flex', justifyContent: 'flex-end' }}>
-			{onFieldOpponents.map((t) => (
-				<img key={t.id} src={getPokemonSprite(t.dexId)} />
-			))}
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'space-between',
+				alignItems: 'center',
+			}}
+		>
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					gap: '.5rem',
+				}}
+			>
+				{onFieldOpponents.map((t) => (
+					<div
+						style={{
+							border: '1px solid black',
+							padding: '0 2rem',
+							borderRadius: 9000,
+						}}
+						key={t.id}
+					>
+						{t.data.name}
+						<HpBar max={t.stats.hp} damage={t.damage} />
+					</div>
+				))}
+			</div>
+			<div>
+				{onFieldOpponents.map((t) => (
+					<img key={t.id} src={getPokemonSprite(t.dexId)} />
+				))}
+			</div>
 		</div>
 	);
 }
--- src/modules/Battle/components/PlayerLane.tsx
+++ src/modules/Battle/components/PlayerLane.tsx
@@ -1,3 +1,4 @@
+import { HpBar } from '../../../components/HpBar/HpBar';
 import { getPokemonSprite } from '../../../functions/getPokemonSprite';
 import { BattlePokemon } from '../../../interfaces/BattlePokemon';
 
@@ -6,11 +7,36 @@ export function PlayerLane(props: { onFieldTeam: BattlePokemon[] }) {
 		<div
 			style={{
 				display: 'flex',
+				justifyContent: 'space-between',
+				alignItems: 'center',
 			}}
 		>
-			{props.onFieldTeam.map((t) => (
-				<img key={t.id} src={getPokemonSprite(t.dexId, 'back')} />
-			))}
+			<div>
+				{props.onFieldTeam.map((t) => (
+					<img key={t.id} src={getPokemonSprite(t.dexId, 'back')} />
+				))}
+			</div>
+			<div
+				style={{
+					display: 'flex',
+					alignItems: 'center',
+					gap: '.5rem',
+				}}
+			>
+				{props.onFieldTeam.map((t) => (
+					<div
+						style={{
+							border: '1px solid black',
+							padding: '0 2rem',
+							borderRadius: 9000,
+						}}
+						key={t.id}
+					>
+						{t.data.name}
+						<HpBar max={t.stats.hp} damage={t.damage} />
+					</div>
+				))}
+			</div>
 		</div>
 	);
 }
