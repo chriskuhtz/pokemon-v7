c074716c389b15c26d0fe1f5536cfe750fc5a16e
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-19 21:30:24 +0100

    add healingitems to actions

--- src/modules/Battle/components/ActionSelection.tsx
+++ src/modules/Battle/components/ActionSelection.tsx
@@ -1,6 +1,7 @@
+import { canBenefitFromItem } from '../../../functions/canBenefitFromItem';
 import { BattlePokemon } from '../../../interfaces/BattlePokemon';
 import { Inventory } from '../../../interfaces/Inventory';
-import { isPokeball } from '../../../interfaces/Item';
+import { isHealingItem, isPokeball } from '../../../interfaces/Item';
 import { ActionType, ChooseActionPayload } from '../BattleField';
 
 export function ActionSelection({
@@ -8,11 +9,13 @@ export function ActionSelection({
 	inventory,
 	setChosenAction,
 	chooseAction,
+	allTargets,
 }: {
 	controlled: BattlePokemon;
 	inventory: Inventory;
 	setChosenAction: (x: ActionType) => void;
 	chooseAction: (x: ChooseActionPayload) => void;
+	allTargets: BattlePokemon[];
 }) {
 	return (
 		<div
@@ -59,7 +62,12 @@ export function ActionSelection({
 					</button>
 				)}
 				{Object.entries(inventory).map(([item, amount]) => {
-					if (amount > 0 && isPokeball(item)) {
+					if (
+						amount > 0 &&
+						(isPokeball(item) ||
+							(isHealingItem(item) &&
+								allTargets.some((t) => canBenefitFromItem(t, item))))
+					) {
 						return (
 							<button onClick={() => setChosenAction(item)} key={item}>
 								{item}
@@ -67,6 +75,7 @@ export function ActionSelection({
 						);
 					}
 				})}
+
 				<button
 					onClick={() =>
 						chooseAction({
