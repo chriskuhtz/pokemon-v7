44ae3af68a53ffdfa0c9031f6b2a275a0e1314a2
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-20 12:29:25 +0100

    adjust can benefit from item

--- src/functions/canBenefitFromItem.ts
+++ src/functions/canBenefitFromItem.ts
@@ -1,12 +1,24 @@
 import { isEqual } from 'lodash';
 import { BattlePokemon } from '../interfaces/BattlePokemon';
-import { HealingItemType } from '../interfaces/Item';
+import {
+	HealingItemType,
+	isPPRestorationItem,
+	PPRestoringItemType,
+} from '../interfaces/Item';
 import { OwnedPokemon } from '../interfaces/OwnedPokemon';
 import { applyItemToPokemon } from './applyItemToPokemon';
+import { getMoveNamesArray } from './changeMovePP';
 
 export function canBenefitFromItem<T extends OwnedPokemon | BattlePokemon>(
 	pokemon: T,
-	item: HealingItemType
+	item: HealingItemType | PPRestoringItemType
 ): boolean {
+	console.log(pokemon, getMoveNamesArray(pokemon));
+	if (
+		isPPRestorationItem(item) &&
+		getMoveNamesArray(pokemon).some((m) => m.usedPP > 0)
+	) {
+		return true;
+	}
 	return !isEqual(pokemon, applyItemToPokemon(pokemon, item));
 }
