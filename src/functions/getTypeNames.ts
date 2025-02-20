a9991d8553e1bf24e5349759bc401406beb9d0b0
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-20 11:43:32 +0100

    correct return type

--- src/functions/getTypeNames.ts
+++ src/functions/getTypeNames.ts
@@ -1,10 +1,11 @@
 import { BattlePokemon, isBattlePokemon } from '../interfaces/BattlePokemon';
 import { OwnedPokemon } from '../interfaces/OwnedPokemon';
 import { PokemonData } from '../interfaces/PokemonData';
+import { PokemonType } from '../interfaces/PokemonType';
 
 export const getTypeNames = (
 	pokemon: BattlePokemon | (OwnedPokemon & { data: PokemonData })
-): string[] => {
+): PokemonType[] => {
 	if (isBattlePokemon(pokemon)) {
 		return pokemon.colorChangedType
 			? [pokemon.colorChangedType]
