4c35aff83e388377501e742d67784659945d58eb
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-21 19:02:13 +0100

    new quest

--- src/constants/checkLists/questsRecord.ts
+++ src/constants/checkLists/questsRecord.ts
@@ -1,9 +1,11 @@
 import { calculateLevelData } from '../../functions/calculateLevelData';
 import { Quest } from '../../interfaces/Quest';
+import { meadow } from '../maps/meadow';
 
 export const questNames = [
 	'catch a pikachu',
 	'Train a Pokemon to lvl 10',
+	'Catch a nocturnal pokemon from the meadow',
 ] as const;
 export type QuestName = (typeof questNames)[number];
 
@@ -20,6 +22,16 @@ export const QuestsRecord: Record<QuestName, Quest> = {
 			return s.pokemon.some((p) => {
 				const { level } = calculateLevelData(p.xp);
 				return level >= 10;
+			});
+		},
+	},
+	'Catch a nocturnal pokemon from the meadow': {
+		rewardItems: { 'dusk-stone': 1 },
+		conditionFunction: (s) => {
+			return s.pokemon.some((p) => {
+				meadow.possibleEncounters.NIGHT.some(
+					(encounter) => encounter.dexId === p.dexId
+				);
 			});
 		},
 	},
