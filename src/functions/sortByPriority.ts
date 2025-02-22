d8f88f65d5c1a4a055d01c6e474d218e350a11d9
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-22 15:39:56 +0100

    schwift schwimm

--- src/functions/sortByPriority.ts
+++ src/functions/sortByPriority.ts
@@ -1,12 +1,14 @@
 import { PARA_SPEED_FACTOR } from '../interfaces/Ailment';
 
 import { BattlePokemon } from '../interfaces/BattlePokemon';
+import { WeatherType } from '../interfaces/Weather';
 import { calculateModifiedStat } from './calculateModifiedStat';
 
 export const sortByPriority = (
 	a: BattlePokemon,
 	b: BattlePokemon,
-	battleRound: number
+	battleRound: number,
+	battleWeather: WeatherType | undefined
 ): number => {
 	const aMove = a.moveQueue.find((m) => m.round === battleRound);
 	const bMove = b.moveQueue.find((m) => m.round === battleRound);
@@ -42,12 +44,23 @@ export const sortByPriority = (
 		return 1;
 	}
 
+	const paraFactorA =
+		a.primaryAilment?.type === 'paralysis' ? PARA_SPEED_FACTOR : 1;
+	const swiftSwimFactorA =
+		a.ability === 'swift-swim' && battleWeather === 'rain' ? 2 : 1;
 	const aSpeed =
 		calculateModifiedStat(a.stats.speed, a.statBoosts.speed) *
-		(a.primaryAilment?.type === 'paralysis' ? PARA_SPEED_FACTOR : 1);
+		paraFactorA *
+		swiftSwimFactorA;
+
+	const paraFactorB =
+		b.primaryAilment?.type === 'paralysis' ? PARA_SPEED_FACTOR : 1;
+	const swiftSwimFactorB =
+		b.ability === 'swift-swim' && battleWeather === 'rain' ? 2 : 1;
 	const bSpeed =
 		calculateModifiedStat(b.stats.speed, b.statBoosts.speed) *
-		(b.primaryAilment?.type === 'paralysis' ? PARA_SPEED_FACTOR : 1);
+		paraFactorB *
+		swiftSwimFactorB;
 
 	if (bSpeed > aSpeed) {
 		return 1;
