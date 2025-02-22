b8dcae0fa36a93e989aa3b9f53f75842ebcee669
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-22 15:56:07 +0100

    random held items

--- src/functions/getPlayerId.ts
+++ src/functions/getPlayerId.ts
@@ -1,9 +1,16 @@
 import { localStorageId } from '../constants/gameData';
-import { SaveFile } from '../interfaces/SaveFile';
+import { SaveFile, SettingsObject } from '../interfaces/SaveFile';
 
 export const getPlayerId = (): string | undefined => {
 	const local = window.localStorage.getItem(localStorageId);
 	const { playerId } = local ? (JSON.parse(local) as SaveFile) : {};
 
 	return playerId;
+};
+
+export const getSettings = (): SettingsObject | undefined => {
+	const local = window.localStorage.getItem(localStorageId);
+	const { settings } = local ? (JSON.parse(local) as SaveFile) : {};
+
+	return settings;
 };
