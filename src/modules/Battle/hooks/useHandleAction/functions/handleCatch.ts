079fb9f2d7c8595084e7c671ee2f9bf2ae9c2efc
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-02-23 12:44:07 +0100

    fix catching

--- src/modules/Battle/hooks/useHandleAction/functions/handleCatch.ts
+++ src/modules/Battle/hooks/useHandleAction/functions/handleCatch.ts
@@ -45,92 +45,121 @@ export const handleCatch = (
 		return;
 	}
 
-	const getCatchStepMessage = (step: 2 | 3): Message => ({
-		message: Array.from({ length: step })
-			.map(() => 'wiggle')
-			.join('...'),
-		onRemoval: () =>
-			setPokemon((pokemon) =>
-				pokemon.map((p) => {
-					if (p.id === attacker.id) {
-						return { ...p, moveQueue: [] };
-					}
-					if (p.id === target.id) {
-						const continueProcess = determineCaptureSuccess(
-							move.ball,
-							target,
-							battleRound,
-							battleLocation
-						);
+	const check1 = determineCaptureSuccess(
+		move.ball,
+		target,
+		battleRound,
+		battleLocation
+	);
+	const check2 = determineCaptureSuccess(
+		move.ball,
+		target,
+		battleRound,
+		battleLocation
+	);
+	const check3 = determineCaptureSuccess(
+		move.ball,
+		target,
+		battleRound,
+		battleLocation
+	);
 
-						if (continueProcess) {
+	const getCatchStepMessage = (step: 1 | 2 | 3): Message => {
+		const m = `${Array.from({ length: step })
+			.map(() => 'wiggle')
+			.join('...')}...`;
+
+		return {
+			message: m,
+			onRemoval: () =>
+				setPokemon((pokemon) =>
+					pokemon.map((p) => {
+						if (p.id === attacker.id) {
+							return { ...p, moveQueue: [] };
+						}
+						if (p.id === target.id) {
 							return {
 								...p,
 
 								status: `CATCHING_${step}`,
 								ball: move.ball,
 							};
-						} else
-							interjectMessage({
-								message: `${target.data.name} broke free`,
-								clearStackOnRemoval: true,
-							});
-						return {
-							...p,
-							status: 'ONFIELD',
-						};
-					}
-
-					return p;
-				})
-			),
-	});
-
-	addMultipleMessages([
-		{
-			message: `You throw a ${move.ball} at ${target.data.name}`,
-			onRemoval: () =>
-				setPokemon((pokemon) =>
-					pokemon.map((p) => {
-						if (p.id === attacker.id) {
-							return { ...p, moveQueue: [] };
-						}
-						if (p.id === target.id) {
-							return {
-								...p,
-								status: 'CATCHING_1',
-								ball: move.ball,
-							};
 						}
 
 						return p;
 					})
 				),
-		},
-		getCatchStepMessage(2),
-		getCatchStepMessage(3),
-		{
-			message: `${target.data.name} was caught`,
-			onRemoval: () =>
-				setPokemon((pokemon) =>
-					pokemon.map((p) => {
-						if (p.id === attacker.id) {
-							return { ...p, moveQueue: [] };
-						}
-						if (p.id === target.id) {
-							return {
-								...p,
-								moveQueue: [],
-								status: 'CAUGHT',
-								ball: move.ball,
-							};
-						}
+		};
+	};
 
-						return p;
-					})
-				),
-		},
-	]);
+	addMultipleMessages(
+		[
+			{
+				message: `You throw a ${move.ball} at ${target.data.name}`,
+				onRemoval: () =>
+					setPokemon((pokemon) =>
+						pokemon.map((p) => {
+							if (p.id === attacker.id) {
+								return { ...p, moveQueue: [] };
+							}
+							if (p.id === target.id) {
+								return {
+									...p,
+
+									status: `CATCHING_1`,
+									ball: move.ball,
+								};
+							}
+
+							return p;
+						})
+					),
+			},
+			check1 ? getCatchStepMessage(2) : undefined,
+			check1 && check2 ? getCatchStepMessage(3) : undefined,
+			check1 && check2 && check3
+				? {
+						message: `${target.data.name} was caught`,
+						onRemoval: () =>
+							setPokemon((pokemon) =>
+								pokemon.map((p) => {
+									if (p.id === attacker.id) {
+										return { ...p, moveQueue: [] };
+									}
+									if (p.id === target.id) {
+										return {
+											...p,
+											moveQueue: [],
+											status: 'CAUGHT',
+											ball: move.ball,
+										};
+									}
+
+									return p;
+								})
+							),
+				  }
+				: {
+						message: `${target.data.name} broke free`,
+						onRemoval: () =>
+							setPokemon((pokemon) =>
+								pokemon.map((p) => {
+									if (p.id === attacker.id) {
+										return { ...p, moveQueue: [] };
+									}
+									if (p.id === target.id) {
+										return {
+											...p,
+											status: 'ONFIELD',
+										};
+									}
+
+									return p;
+								})
+							),
+				  },
+		].filter((m) => m !== undefined)
+	);
 
 	return;
 };
