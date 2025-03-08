0785e82636b9f72b5673d81d05c9fe57ef218148
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-03-08 22:11:33 +0100

    honey quest

--- src/constants/checkLists/questsRecord.ts
+++ src/constants/checkLists/questsRecord.ts
@@ -1,6 +1,9 @@
 import { Quest } from '../../interfaces/Quest';
 
-export const questNames = ['catch a pikachu'] as const;
+export const questNames = [
+	'catch a pikachu',
+	'lure a pokemon with honey',
+] as const;
 export type QuestName = (typeof questNames)[number];
 
 export const QuestsRecord: Record<QuestName, Quest> = {
@@ -8,6 +11,12 @@ export const QuestsRecord: Record<QuestName, Quest> = {
 		rewardItems: { 'thunder-stone': 1 },
 		conditionFunction: (s) => {
 			return s.pokemon.some((p) => p.dexId === 25);
+		},
+	},
+	'lure a pokemon with honey': {
+		rewardItems: { 'sitrus-berry': 3 },
+		conditionFunction: (s) => {
+			return s.mileStones.hasCaughtAPokemonWithHoney;
 		},
 	},
 };
