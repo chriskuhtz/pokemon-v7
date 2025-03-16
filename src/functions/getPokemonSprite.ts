b89a88d614b3cfda44b49379888bdf39d33d5a25
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-03-16 07:08:05 +0100

    remove fallbacks

--- src/functions/getPokemonSprite.ts
+++ src/functions/getPokemonSprite.ts
@@ -1,18 +1,14 @@
 import { nameToIdMap, PokemonName } from '../constants/pokemonNames';
 
-export const getPokemonSprite = (
-	name: PokemonName,
-	mode?: 'back',
-	gif?: boolean
-): string => {
+export const getPokemonSprite = (name: PokemonName, mode?: 'back'): string => {
 	const id = nameToIdMap[name];
-	if (gif) {
-		return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${
-			mode ? `${mode}/` : ''
-		}${id}.gif`;
-	}
-	if (mode === 'back') {
-		return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`;
-	}
-	return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+	//if (gif) {
+	return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${
+		mode ? `${mode}/` : ''
+	}${id}.gif`;
+	// }
+	// if (mode === 'back') {
+	// 	return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${id}.png`;
+	// }
+	// return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 };
