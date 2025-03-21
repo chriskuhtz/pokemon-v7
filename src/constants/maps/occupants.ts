26249182e8007ad5c2f3fefe299405b0eee4552c
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-03-21 02:57:32 +0100

    route S1

--- src/constants/maps/occupants.ts
+++ src/constants/maps/occupants.ts
@@ -186,6 +186,34 @@ export const routeS1E1Occupants: OverworldMap['occupants'] = [
 ];
 export const routeS1Occupants: OverworldMap['occupants'] = [
 	{
+		type: 'ON_STEP_PORTAL',
+		x: 50,
+		y: 25,
+		portal: {
+			mapId: 'routeS1E1',
+			x: 1,
+			y: 25,
+			orientation: 'RIGHT',
+			forwardFoot: 'CENTER1',
+		},
+		conditionFunction: () => true,
+		id: 'routeS1_to_routeS1E1',
+	},
+	{
+		type: 'ON_STEP_PORTAL',
+		x: 0,
+		y: 25,
+		portal: {
+			mapId: 'routeS1W1',
+			y: 25,
+			x: 49,
+			orientation: 'LEFT',
+			forwardFoot: 'CENTER1',
+		},
+		conditionFunction: () => true,
+		id: 'routeS1_to_routeS1W1',
+	},
+	{
 		type: 'HALLOWED_TOWER',
 		x: 47,
 		y: 40,
