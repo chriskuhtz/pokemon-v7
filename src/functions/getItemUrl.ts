ce84ba6692a7ce70da775ffb1b6579e5dfa17fb7
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-03-23 22:25:02 +0100

    berry fallbacks

--- src/functions/getItemUrl.ts
+++ src/functions/getItemUrl.ts
@@ -1,4 +1,17 @@
 import { ItemType } from '../interfaces/Item';
 
-export const getItemUrl = (item: ItemType): string =>
-	`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${item}.png`;
+const customSprites: Partial<Record<ItemType, string>> = {
+	'golden-razz-berry': '/mapObjects/pokeball.png',
+	'golden-pinap-berry': '/mapObjects/pokeball.png',
+	'golden-nanab-berry': '/mapObjects/pokeball.png',
+	'silver-razz-berry': '/mapObjects/pokeball.png',
+	'silver-pinap-berry': '/mapObjects/pokeball.png',
+	'silver-nanab-berry': '/mapObjects/pokeball.png',
+};
+export const getItemUrl = (item: ItemType): string => {
+	if (customSprites[item]) {
+		return customSprites[item];
+	}
+
+	return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/items/${item}.png`;
+};
