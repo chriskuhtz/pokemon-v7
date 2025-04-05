f7762886c9c2777dc5b1ee3e32e50f020e34e152
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-04-05 14:36:45 +0200

    custom items

--- src/functions/getItemUrl.ts
+++ src/functions/getItemUrl.ts
@@ -1,13 +1,14 @@
 import { ItemType } from '../interfaces/Item';
 
 const customSprites: Partial<Record<ItemType, string>> = {
-	'golden-razz-berry': '/mapObjects/pokeball.png',
-	'golden-pinap-berry': '/mapObjects/pokeball.png',
-	'golden-nanab-berry': '/mapObjects/pokeball.png',
-	'silver-razz-berry': '/mapObjects/pokeball.png',
-	'silver-pinap-berry': '/mapObjects/pokeball.png',
-	'silver-nanab-berry': '/mapObjects/pokeball.png',
+	'golden-razz-berry': '/customItems/golden-razz-berry.png',
+	'golden-pinap-berry': '/customItems/golden-pinap-berry.png',
+	'golden-nanab-berry': '/customItems/golden-nanab-berry.png',
+	'silver-razz-berry': '/customItems/silver-razz-berry.png',
+	'silver-pinap-berry': '/customItems/silver-pinap-berry.png',
+	'silver-nanab-berry': '/customItems/silver-nanab-berry.png',
 	'black-augurite': '/customItems/black-augurite.png',
+	'peat-block': '/customItems/peat-block.png',
 };
 export const getItemUrl = (item: ItemType): string => {
 	if (customSprites[item]) {
