89f0fe2f9a348d5afd566e31f735116accb1f95e
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-04-17 12:31:59 +0200

    encounters for cave

---
+++ src/constants/maps/encounters/caveW1.ts
@@ -0,0 +1,53 @@
+import { EncounterMap } from '../encounters';
+
+export const caveW1Encounters: EncounterMap = {
+	WATER: [],
+	BASE: [
+		{
+			name: 'golem',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'common',
+		},
+		{
+			name: 'golbat',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'common',
+		},
+		{
+			name: 'crobat',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'ultra-rare',
+		},
+		{
+			name: 'kangaskhan',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'ultra-rare',
+		},
+		{
+			name: 'parasect',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'medium',
+		},
+		{
+			name: 'sableye',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'rare',
+		},
+		{
+			name: 'gigalith',
+			maxXp: 64000,
+			minXp: 42875,
+			rarity: 'medium',
+		},
+	],
+	MORNING: [],
+	DAY: [],
+	EVENING: [],
+	NIGHT: [],
+};
