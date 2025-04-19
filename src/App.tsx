fafc86d476f5c2bc0caade63c110798b93f3fd97
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-04-19 10:12:17 +0200

    Intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useContext, useMemo, useState } from 'react';
+import { Sprite } from './components/Sprite/Sprite';
 import { MapId, mapsRecord } from './constants/maps/mapsRecord';
 import { MessageQueueContext } from './hooks/useMessageQueue';
 import { SaveFileContext } from './hooks/useSaveFile';
@@ -37,6 +38,8 @@ import { Team } from './modules/Team/Team';
 import { TrainingField } from './modules/TrainingField/TrainingField';
 import { VilePlumeScentResearcher } from './modules/VilePlumeScentResearcher/VilePlumeScentResearcher';
 import { Wiki } from './modules/Wiki/Wiki';
+import { Page } from './uiComponents/Page/Page';
+import { Stack } from './uiComponents/Stack/Stack';
 
 export const FullScreenToggle = () => {
 	const [f, setF] = useState<boolean>(!!document.fullscreenElement);
@@ -105,6 +108,14 @@ export const App = (): JSX.Element => {
 
 	// 	return 1;
 	// }, [firstTeamMember.ability]);
+
+	const [hasReadIntro, setHasReadIntro] = useState<boolean>(
+		!!window.localStorage.getItem('hasReadIntro')
+	);
+
+	if (!hasReadIntro) {
+		return <Intro setHasReadIntro={setHasReadIntro} />;
+	}
 
 	if (activeTab === 'BATTLE' && currentChallenger) {
 		return (
@@ -263,5 +274,82 @@ export const App = (): JSX.Element => {
 				setCurrentMarketInventory(i);
 			}}
 		/>
+	);
+};
+
+const Intro = ({
+	setHasReadIntro,
+}: {
+	setHasReadIntro: (x: boolean) => void;
+}) => {
+	return (
+		<Page headline="">
+			<div style={{ padding: '2rem' }}>
+				<Stack alignItems="center" mode="column">
+					<div>
+						<Sprite
+							canvasKey={'yaya'}
+							rotating={false}
+							key={'yaya'}
+							id={`136`}
+						/>
+					</div>
+					<h3>Welcome to Pokemon Kuma</h3>
+					<ol style={{ lineHeight: '1.5rem' }}>
+						<li>
+							<strong>
+								This Single Player Game runs in your Browser and saves to your
+								localstorage.
+							</strong>
+						</li>
+						<li>
+							<strong>There are no Accounts, Tracking, Cookies, etc.</strong>
+						</li>
+						<li>
+							<strong>
+								For obvious reasons, there is also no form of monetization.
+							</strong>
+						</li>
+						<li>
+							<strong>
+								The recommended Screen is a sideways smartphone, but all screen
+								sizes are supported.
+							</strong>
+						</li>
+						<li>
+							<strong>
+								Keyboard controls are also supported, but might be janky at
+								points.
+							</strong>
+						</li>
+						<li>
+							<strong>You can report Bugs and Feedback in the Menu.</strong>
+						</li>
+						<li>
+							<strong>
+								If I know you personally, please choose a name i can recognize,
+								in case i have questions about your reports.
+							</strong>
+						</li>
+						<li>
+							<strong>
+								The Game is in active development, but i dont expect breaking
+								changes anymore, so you should never have to reset your savefile
+								because of updates.
+							</strong>
+						</li>
+					</ol>
+
+					<button
+						onClick={() => {
+							setHasReadIntro(true);
+							window.localStorage.setItem('hasReadIntro', 'true');
+						}}
+					>
+						Got it, lets go
+					</button>
+				</Stack>
+			</div>
+		</Page>
 	);
 };
