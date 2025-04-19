c47e444d35184d7b6dea22a2af1ebe3c1b373eed
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-04-19 20:05:28 +0200

    v0.8

--- src/modules/ChangeLog/ChangeLog.tsx
+++ src/modules/ChangeLog/ChangeLog.tsx
@@ -3,7 +3,7 @@ import { SpriteEnum } from '../../interfaces/SpriteEnum';
 import { Page } from '../../uiComponents/Page/Page';
 import { Stack } from '../../uiComponents/Stack/Stack';
 
-export const newestChangeLog = '0.7';
+export const newestChangeLog = '0.8';
 
 export const ChangeLog = ({
 	setHasReadIntro,
@@ -31,6 +31,7 @@ export const ChangeLog = ({
 					>
 						Got it, lets go
 					</button>
+					<ChangeLogV08 />
 					<ChangeLogV07 />
 					<ChangeLogV06 />
 					<ChangeLogV05 />
@@ -38,6 +39,15 @@ export const ChangeLog = ({
 				</Stack>
 			</div>
 		</Page>
+	);
+};
+export const ChangeLogV08 = (): JSX.Element => {
+	return (
+		<>
+			<h3>V 0.8:</h3>
+
+			<ol style={{ lineHeight: '1.5rem' }}></ol>
+		</>
 	);
 };
 export const ChangeLogV07 = (): JSX.Element => {
