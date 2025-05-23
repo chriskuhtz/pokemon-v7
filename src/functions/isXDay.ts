33c86ecf86d9505cd090c75b6d904557f96ba40c
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-05-24 01:12:30 +0200

    actually fix days

--- src/functions/isXDay.ts
+++ src/functions/isXDay.ts
@@ -11,13 +11,13 @@ export const isWednesday = () => {
 	return new Date().getDay() === 3;
 };
 export const isThursday = () => {
-	return new Date().getDay() === 0;
+	return new Date().getDay() === 4;
 };
 export const isFriday = () => {
-	return new Date().getDay() === 0;
+	return new Date().getDay() === 5;
 };
 export const isSaturday = () => {
-	return new Date().getDay() === 0;
+	return new Date().getDay() === 6;
 };
 
 export const isOddDay = () => {
