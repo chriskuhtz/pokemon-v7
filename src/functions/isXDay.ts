67f2d6791e31ca79a0c6fd211a8551a463819a4d
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-05-24 01:12:04 +0200

    fix days

--- src/functions/isXDay.ts
+++ src/functions/isXDay.ts
@@ -1,23 +1,23 @@
 export const isSunday = () => {
-	return true; // new Date().getDay() === 0;
+	return new Date().getDay() === 0;
 };
 export const isMonday = () => {
-	return true; // new Date().getDay() === 1;
+	return new Date().getDay() === 1;
 };
 export const isTuesday = () => {
-	return true; // new Date().getDay() === 2;
+	return new Date().getDay() === 2;
 };
 export const isWednesday = () => {
-	return true; // new Date().getDay() === 3;
+	return new Date().getDay() === 3;
 };
 export const isThursday = () => {
-	return true; // new Date().getDay() === 0;
+	return new Date().getDay() === 0;
 };
 export const isFriday = () => {
-	return true; // new Date().getDay() === 0;
+	return new Date().getDay() === 0;
 };
 export const isSaturday = () => {
-	return true; // new Date().getDay() === 0;
+	return new Date().getDay() === 0;
 };
 
 export const isOddDay = () => {
