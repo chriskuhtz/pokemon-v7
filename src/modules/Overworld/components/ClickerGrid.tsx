b2343dcaeceb6a66c1ed1eb57fa11ef6c211ea01
Jan Krause <jan-krause1@hotmail.de>
2025-05-23 20:48:43 +0200

    added encounters to routeS1 bug fix: overrunning into the next map, when changing between maps

--- src/modules/Overworld/components/ClickerGrid.tsx
+++ src/modules/Overworld/components/ClickerGrid.tsx
@@ -1,39 +1,42 @@
-import React from 'react';
+import React from "react";
+import { MapId } from "../../../constants/maps/mapsRecord";
 
 const unmemoedClickerGrid = ({
-	width,
-	height,
-	onClick,
-	baseSize,
+  width,
+  height,
+  onClick,
+  baseSize,
+  mapId,
 }: {
-	width: number;
-	height: number;
-	onClick: (update: { y: number; x: number }) => void;
-	baseSize: number;
+  width: number;
+  height: number;
+  onClick: (update: { y: number; x: number; mapId: MapId }) => void;
+  baseSize: number;
+  mapId: MapId;
 }) => {
-	return (
-		<div
-			style={{
-				display: 'grid',
-				gridTemplateColumns: Array.from({ length: width })
-					.map(() => '1fr')
-					.join(' '),
-			}}
-		>
-			{Array.from({ length: height }).map((_, h) => {
-				return Array.from({ length: width }).map((_, w) => (
-					<div
-						key={h + '+' + w}
-						onClick={() => onClick({ y: h, x: w })}
-						style={{
-							width: baseSize,
-							height: baseSize,
-						}}
-					></div>
-				));
-			})}
-		</div>
-	);
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: Array.from({ length: width })
+          .map(() => "1fr")
+          .join(" "),
+      }}
+    >
+      {Array.from({ length: height }).map((_, h) => {
+        return Array.from({ length: width }).map((_, w) => (
+          <div
+            key={h + "+" + w}
+            onClick={() => onClick({ y: h, x: w, mapId })}
+            style={{
+              width: baseSize,
+              height: baseSize,
+            }}
+          ></div>
+        ));
+      })}
+    </div>
+  );
 };
 
 export const ClickerGrid = React.memo(unmemoedClickerGrid);
