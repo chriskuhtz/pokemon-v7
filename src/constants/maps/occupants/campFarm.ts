be463517f9ea82c701f11c1786119fdbcab51e37
Jan Krause <jan-krause1@hotmail.de>
2025-06-02 15:31:46 +0200

    reworked Farm part of camp, added grey apricorns that can be turned into pokeballs

--- src/constants/maps/occupants/campFarm.ts
+++ src/constants/maps/occupants/campFarm.ts
@@ -1,30 +1,36 @@
+import { makeApricornTree } from "../../../functions/makeApricornTree";
 import { dugtrioExplorers } from "../../../hooks/useDugtrioExplorers";
 import { zigzagoonForagers } from "../../../hooks/useZigzagoonForagers";
 import { Occupant, OverworldMap } from "../../../interfaces/OverworldMap";
 import { SpriteEnum } from "../../../interfaces/SpriteEnum";
 import { amoongussCompostResearchers } from "../../../modules/AmoongussResearcher/AmoongussResearcher";
-import { dragoniteTaxi } from "../../../modules/DragoniteTaxi/DragoniteTaxi";
 import { miltankFarm } from "../../../modules/MiltankFarm/MiltankFarm";
 import { seedvaultResearcher } from "../../../modules/SeedVault/SeedVault";
 import { vileplumeResearchers } from "../../../modules/VilePlumeScentResearcher/VilePlumeScentResearcher";
-import { bugsy } from "./bugsy";
-import { rewardChris } from "./champChris";
-import { chuckLine } from "./chuckLine";
-import { elmLine } from "./elmLine";
-import { lookerLine } from "./lookerLine";
-import { mortyLine } from "./mortyLine";
-import { rowanLine } from "./rowanLine";
-import { whitneyLine } from "./whitney";
 
 const trainingField: Occupant[] = [];
 
 export const campOccupants: OverworldMap["occupants"] = [
   {
+    type: "ON_STEP_PORTAL",
+    id: "camp_to_farm",
+    conditionFunction: () => true,
+    x: 0,
+    y: 3,
+    portal: {
+      mapId: "camp",
+      x: 18,
+      y: 16,
+      orientation: "LEFT",
+      forwardFoot: "CENTER1",
+    },
+  },
+  {
     type: "ROUTER_NPC",
     to: "FARM",
     id: "berry_farmer",
-    x: 15,
-    y: 17,
+    x: 9,
+    y: 14,
     orientation: "UP",
     dialogue: ["We can grow all types of berries and apricorns"],
     sprite: SpriteEnum.farmer,
@@ -32,18 +38,10 @@ export const campOccupants: OverworldMap["occupants"] = [
   },
   {
     type: "COMBEE_HIVE",
-    x: 16,
-    y: 17,
+    x: 8,
+    y: 2,
     id: "combee_hive",
     conditionFunction: (s) => s.campUpgrades["build combee hive"],
-  },
-  {
-    type: "PC",
-    x: 8,
-    y: 7,
-
-    id: "camp_pc",
-    conditionFunction: () => true,
   },
   {
     type: "STORAGE_CHEST",
@@ -56,9 +54,9 @@ export const campOccupants: OverworldMap["occupants"] = [
     type: "ROUTER_NPC",
     to: "APRICORN_SMITHY",
     sprite: SpriteEnum.kurt,
-    x: 3,
-    y: 11,
-    orientation: "DOWN",
+    x: 2,
+    y: 7,
+    orientation: "RIGHT",
     id: "campUpgrade_kurt",
     dialogue: [
       "My name is kurt",
@@ -71,9 +69,9 @@ export const campOccupants: OverworldMap["occupants"] = [
   {
     type: "ROUTER_NPC",
     to: "CHEF_GRANDMA",
-    orientation: "DOWN",
+    orientation: "RIGHT",
     y: 11,
-    x: 4,
+    x: 1,
     dialogue: [
       "Some Berries are not very useful raw",
       "but they are still good ingredients",
@@ -82,19 +80,54 @@ export const campOccupants: OverworldMap["occupants"] = [
     conditionFunction: (s) => s.campUpgrades["invite chef grandma"],
     id: "chef grandma",
   },
-  bugsy,
-  ...whitneyLine,
-  ...mortyLine,
-  ...chuckLine,
-  ...elmLine,
-  ...rowanLine,
+  {
+    type: "POKEMON",
+    dexId: 77,
+    x: 15,
+    y: 9,
+    orientation: "DOWN",
+    id: "ponyta",
+    dialogue: ["Ponyta is trotting happily"],
+    conditionFunction: () => true,
+  },
+  {
+    type: "POKEMON",
+    dexId: 78,
+    x: 17,
+    y: 10,
+    orientation: "RIGHT",
+    id: "rapidash",
+    dialogue: ["Rapidash is trotting majestical"],
+    conditionFunction: () => true,
+  },
+  ...makeApricornTree({
+    x: 1,
+    y: 6,
+    apricorn: "grey-apricorn",
+    id: "farm_grey_tree_1",
+  }),
+  ...makeApricornTree({
+    x: 1,
+    y: 8,
+    apricorn: "grey-apricorn",
+    id: "farm_grey_tree_2",
+  }),
+  ...makeApricornTree({
+    x: 2,
+    y: 5,
+    apricorn: "grey-apricorn",
+    id: "farm_grey_tree_3",
+  }),
+  ...makeApricornTree({
+    x: 2,
+    y: 9,
+    apricorn: "grey-apricorn",
+    id: "farm_grey_tree_4",
+  }),
   ...miltankFarm,
   ...zigzagoonForagers,
   ...dugtrioExplorers,
   ...vileplumeResearchers,
   ...amoongussCompostResearchers,
   ...seedvaultResearcher,
-  ...dragoniteTaxi,
-  rewardChris,
-  ...lookerLine,
 ];
--- src/modules/AmoongussResearcher/AmoongussResearcher.tsx
+++ src/modules/AmoongussResearcher/AmoongussResearcher.tsx
@@ -1,122 +1,122 @@
-import { useCallback, useContext, useMemo } from 'react';
-import { ItemSprite } from '../../components/ItemSprite/ItemSprite';
-import { lettersToNumbers } from '../../constants/lettersToNumbers';
-import { MessageQueueContext } from '../../hooks/useMessageQueue';
-import { useNavigate } from '../../hooks/useNavigate';
-import { SaveFileContext } from '../../hooks/useSaveFile';
-import { joinInventories } from '../../interfaces/Inventory';
+import { useCallback, useContext, useMemo } from "react";
+import { ItemSprite } from "../../components/ItemSprite/ItemSprite";
+import { lettersToNumbers } from "../../constants/lettersToNumbers";
+import { MessageQueueContext } from "../../hooks/useMessageQueue";
+import { useNavigate } from "../../hooks/useNavigate";
+import { SaveFileContext } from "../../hooks/useSaveFile";
+import { joinInventories } from "../../interfaces/Inventory";
 import {
-	ItemType,
-	MulchType,
-	isApricorn,
-	isBerry,
-	mulches,
-} from '../../interfaces/Item';
-import { Occupant } from '../../interfaces/OverworldMap';
-import { SpriteEnum } from '../../interfaces/SpriteEnum';
-import { Card } from '../../uiComponents/Card/Card';
-import { Page } from '../../uiComponents/Page/Page';
-import { Stack } from '../../uiComponents/Stack/Stack';
+  ItemType,
+  MulchType,
+  isApricorn,
+  isBerry,
+  mulches,
+} from "../../interfaces/Item";
+import { Occupant } from "../../interfaces/OverworldMap";
+import { SpriteEnum } from "../../interfaces/SpriteEnum";
+import { Card } from "../../uiComponents/Card/Card";
+import { Page } from "../../uiComponents/Page/Page";
+import { Stack } from "../../uiComponents/Stack/Stack";
 
 export const useAmoongussResearcher = () => {
-	const { patchSaveFileReducer, saveFile } = useContext(SaveFileContext);
-	const { addMessage } = useContext(MessageQueueContext);
+  const { patchSaveFileReducer, saveFile } = useContext(SaveFileContext);
+  const { addMessage } = useContext(MessageQueueContext);
 
-	const options: [ItemType, number][] = useMemo(() => {
-		return Object.entries(saveFile.bag).filter(
-			([item, amount]) => (isApricorn(item) || isBerry(item)) && amount > 0
-		) as [ItemType, number][];
-	}, [saveFile.bag]);
+  const options: [ItemType, number][] = useMemo(() => {
+    return Object.entries(saveFile.bag).filter(
+      ([item, amount]) => (isApricorn(item) || isBerry(item)) && amount > 0
+    ) as [ItemType, number][];
+  }, [saveFile.bag]);
 
-	const trade = useCallback(
-		(item: ItemType, mulch: MulchType) => {
-			if (!(isApricorn(item) || isBerry(item))) {
-				return;
-			}
+  const trade = useCallback(
+    (item: ItemType, mulch: MulchType) => {
+      if (!(isApricorn(item) || isBerry(item))) {
+        return;
+      }
 
-			addMessage({ message: `Traded 1 ${item} for 1 ${mulch}` });
-			patchSaveFileReducer({
-				bag: joinInventories(saveFile.bag, {
-					[mulch]: 1,
-					[item]: -1,
-				}),
-			});
-		},
-		[addMessage, patchSaveFileReducer, saveFile.bag]
-	);
+      addMessage({ message: `Traded 1 ${item} for 1 ${mulch}` });
+      patchSaveFileReducer({
+        bag: joinInventories(saveFile.bag, {
+          [mulch]: 1,
+          [item]: -1,
+        }),
+      });
+    },
+    [addMessage, patchSaveFileReducer, saveFile.bag]
+  );
 
-	return { options, trade };
+  return { options, trade };
 };
 
 export const AmoongussCompostResearcher = (): JSX.Element => {
-	const { trade, options } = useAmoongussResearcher();
+  const { trade, options } = useAmoongussResearcher();
 
-	const navigate = useNavigate();
-	return (
-		<Page
-			headline="Trade Berries or Apricorns for Mulches"
-			goBack={() => navigate('CURATOR', 'OVERWORLD')}
-		>
-			<Stack mode="column">
-				{options.length > 0
-					? options.map(([item, amount]) => {
-							const nameToNumber = item
-								.split('')
-								.reduce((sum, summand) => sum + lettersToNumbers[summand], 0);
+  const navigate = useNavigate();
+  return (
+    <Page
+      headline="Trade Berries or Apricorns for Mulches"
+      goBack={() => navigate("CURATOR", "OVERWORLD")}
+    >
+      <Stack mode="column">
+        {options.length > 0
+          ? options.map(([item, amount]) => {
+              const nameToNumber = item
+                .split("")
+                .reduce((sum, summand) => sum + lettersToNumbers[summand], 0);
 
-							const index = nameToNumber % mulches.length;
-							const randomMulch = mulches[index];
-							return (
-								<Card
-									key={item}
-									onClick={() => trade(item, randomMulch)}
-									icon={<ItemSprite item={item} />}
-									content={
-										<h3>
-											Trade {item}({amount} in Bag) for {randomMulch}
-										</h3>
-									}
-									actionElements={[<ItemSprite item={randomMulch} />]}
-								/>
-							);
-					  })
-					: [
-							<h3 key="empty">
-								Amoongus can break down berries or apricorns into mulch
-							</h3>,
-					  ]}
-			</Stack>
-		</Page>
-	);
+              const index = nameToNumber % mulches.length;
+              const randomMulch = mulches[index];
+              return (
+                <Card
+                  key={item}
+                  onClick={() => trade(item, randomMulch)}
+                  icon={<ItemSprite item={item} />}
+                  content={
+                    <h3>
+                      Trade {item}({amount} in Bag) for {randomMulch}
+                    </h3>
+                  }
+                  actionElements={[<ItemSprite item={randomMulch} />]}
+                />
+              );
+            })
+          : [
+              <h3 key="empty">
+                Amoongus can break down berries or apricorns into mulch
+              </h3>,
+            ]}
+      </Stack>
+    </Page>
+  );
 };
 
 export const amoongussCompostResearchers: Occupant[] = [
-	{
-		type: 'ROUTER_NPC',
-		dialogue: [
-			'I am researching compost',
-			'amoonguss is really efficient at it',
-			'If you bring me plants',
-			'It will turn them into mulch in no time',
-		],
-		to: 'AMOONGUSS',
-		x: 6,
-		y: 13,
-		orientation: 'UP',
-		sprite: SpriteEnum.scientistFemale,
-		id: 'amoonguss researcher',
-		conditionFunction: (s) =>
-			s.campUpgrades['invite amoongus compost researcher'],
-	},
-	{
-		type: 'POKEMON',
-		dexId: 591,
-		x: 7,
-		y: 13,
-		orientation: 'UP',
-		id: 'amoonguss',
-		dialogue: ["Amoonguss is 'processing' berries"],
-		conditionFunction: (s) =>
-			s.campUpgrades['invite amoongus compost researcher'],
-	},
+  {
+    type: "ROUTER_NPC",
+    dialogue: [
+      "I am researching compost",
+      "amoonguss is really efficient at it",
+      "If you bring me plants",
+      "It will turn them into mulch in no time",
+    ],
+    to: "AMOONGUSS",
+    x: 1,
+    y: 14,
+    orientation: "RIGHT",
+    sprite: SpriteEnum.scientistFemale,
+    id: "amoonguss researcher",
+    conditionFunction: (s) =>
+      s.campUpgrades["invite amoongus compost researcher"],
+  },
+  {
+    type: "POKEMON",
+    dexId: 591,
+    x: 1,
+    y: 13,
+    orientation: "RIGHT",
+    id: "amoonguss",
+    dialogue: ["Amoonguss is 'processing' berries"],
+    conditionFunction: (s) =>
+      s.campUpgrades["invite amoongus compost researcher"],
+  },
 ];
