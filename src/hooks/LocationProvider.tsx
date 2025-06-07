2ce8c4b8076286e4e3ed24dfe2021d183fc6f405
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-06-07 07:55:41 +0200

    rescue jan

--- src/hooks/LocationProvider.tsx
+++ src/hooks/LocationProvider.tsx
@@ -44,6 +44,12 @@ export const LocationProvider = ({ children }: { children: ReactNode }) => {
 		[location.mapId, patchSaveFileReducer, saveFile.handledOccupants]
 	);
 
+	//RESCUE FROM SOFT LOCK
+	useEffect(() => {
+		if (saveFile.playerId.includes('jan')) {
+			setLocation(startingLocation);
+		}
+	}, [location, saveFile.playerId, setLocation]);
 	//SYNC WITH LOCAL STORAGE
 	useEffect(() => {
 		window.localStorage.setItem(
