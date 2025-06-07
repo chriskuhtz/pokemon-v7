2d0ca03ffb39aa3b1d170b89faf16436b2abfa4a
Chris Kuhtz <christopherkuhtz@gmail.com>
2025-06-07 16:15:30 +0200

    refine opponent decisions

--- src/functions/determineHighestDamage.ts
+++ src/functions/determineHighestDamage.ts
@@ -1,10 +1,11 @@
 import { firstTurnMoves } from '../constants/groupedMoves';
-import { BattlePokemon, BattleMove } from '../interfaces/BattlePokemon';
+import { BattleMove, BattlePokemon } from '../interfaces/BattlePokemon';
 import { WeatherType } from '../interfaces/Weather';
-import { BattleFieldEffect, ActionType } from '../modules/Battle/BattleField';
+import { ActionType, BattleFieldEffect } from '../modules/Battle/BattleField';
 import { BattleTerrain } from '../modules/Battle/hooks/useBattleTerrain';
 import { calculateDamage } from './calculateDamage';
 import { determineMultiHits } from './determineMultiHits';
+import { getRandomEntry } from './filterTargets';
 import { getHeldItem } from './getHeldItem';
 
 export const determineHighestDamage = (
@@ -14,52 +15,70 @@ export const determineHighestDamage = (
 	weather: WeatherType | undefined,
 	terrain: BattleTerrain | undefined,
 	effects: BattleFieldEffect[]
-): { actionName: ActionType; targetId: string } => {
+): { actionName: ActionType; targetId: string; willBeDefeated: boolean } => {
 	if (targets.length === 0) {
-		return { actionName: 'LOAFING', targetId: attacker.id };
+		return {
+			actionName: 'LOAFING',
+			targetId: attacker.id,
+			willBeDefeated: false,
+		};
 	}
-	const mapped: { actionName: ActionType; targetId: string; damage: number }[] =
-		moves.flatMap((move) => {
-			if (firstTurnMoves.includes(move.name) && attacker.roundsInBattle !== 1) {
-				return [
-					{
-						actionName: move.name,
-						targetId: targets.at(0)?.id ?? '',
-						damage: 0,
-					},
-				];
-			}
-			return targets.map((target) => ({
+	const mapped: {
+		actionName: ActionType;
+		targetId: string;
+		damage: number;
+		willBeDefeated: boolean;
+	}[] = moves.flatMap((move) => {
+		if (firstTurnMoves.includes(move.name) && attacker.roundsInBattle !== 1) {
+			return [
+				{
+					actionName: move.name,
+					targetId: targets.at(0)?.id ?? '',
+					damage: 0,
+					willBeDefeated: false,
+				},
+			];
+		}
+		return targets.map((target) => {
+			const damage = calculateDamage(
+				attacker,
+				target,
+				{
+					name: move.name,
+					type: 'BattleAttack',
+					round: 0,
+					data: move.data,
+					targetId: target.id,
+					multiHits: determineMultiHits(
+						move.data,
+						attacker.ability,
+						getHeldItem(attacker)
+					),
+					isAMultiHit: false,
+				},
+				weather,
+				effects,
+				terrain,
+				false,
+				false,
+				false,
+				false,
+				1,
+				() => {}
+			).damage;
+			return {
 				actionName: move.name,
 				targetId: target.id,
-				damage: calculateDamage(
-					attacker,
-					target,
-					{
-						name: move.name,
-						type: 'BattleAttack',
-						round: 0,
-						data: move.data,
-						targetId: target.id,
-						multiHits: determineMultiHits(
-							move.data,
-							attacker.ability,
-							getHeldItem(attacker)
-						),
-						isAMultiHit: false,
-					},
-					weather,
-					effects,
-					terrain,
-					false,
-					false,
-					false,
-					false,
-					1,
-					() => {}
-				).damage,
-			}));
+				damage,
+				willBeDefeated: target.damage + damage >= target.stats.hp,
+			};
 		});
+	});
+
+	const possibleKills = mapped.filter((m) => m.willBeDefeated);
+	if (possibleKills.length > 0) {
+		return getRandomEntry(possibleKills);
+	}
 
 	const sorted = mapped.sort((a, b) => b.damage - a.damage);
 
