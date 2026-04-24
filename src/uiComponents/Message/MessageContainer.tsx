85b25c3584df24f6207fcb7f91b0b131b899ac3e
Chris Kuhtz <christopherkuhtz@gmail.com>
2026-04-24 02:18:06 +0200

    code cleanup

--- src/uiComponents/Message/MessageContainer.tsx
+++ src/uiComponents/Message/MessageContainer.tsx
@@ -1,60 +1,71 @@
-import { useContext } from 'react';
-import { ErrorBoundary } from 'react-error-boundary';
-import { FaRegCircleCheck } from 'react-icons/fa6';
-import { battleSpriteSize } from '../../constants/gameData/gameData';
-import { LocationProvider } from '../../hooks/LocationProvider';
-import { BaseSizeProvider } from '../../hooks/useBaseSize';
-import { GameDataContext } from '../../hooks/useGameData';
-import { MessageQueueContext } from '../../hooks/useMessageQueue';
-import { SaveFileProvider } from '../../hooks/useSaveFile';
-import { Router } from '../../modules/Router/Router';
-import { Banner } from '../Banner/Banner';
-import { ScreenTransitionProvider } from '../../hooks/useScreenTransitionEffects';
+import { useContext } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { FaRegCircleCheck } from "react-icons/fa6";
+import { battleSpriteSize } from "../../constants/gameData/gameData";
+import { LocationProvider } from "../../hooks/LocationProvider";
+import { BaseSizeProvider } from "../../hooks/useBaseSize";
+import { GameDataContext } from "../../hooks/useGameData";
+import { Message, MessageQueueContext } from "../../hooks/useMessageQueue";
+import { SaveFileProvider } from "../../hooks/useSaveFile";
+import { ScreenTransitionProvider } from "../../hooks/useScreenTransitionEffects";
+import { Router } from "../../modules/Router/Router";
+import { Banner } from "../Banner/Banner";
 
 export const MessageContainer = () => {
-	const { confirmLatestMessage, addMessage, latestMessage } =
-		useContext(MessageQueueContext);
-	const { allowedBaseSizes } = useContext(GameDataContext);
+  const { confirmLatestMessage, addMessage, latestMessage } =
+    useContext(MessageQueueContext);
+  const { allowedBaseSizes } = useContext(GameDataContext);
 
-	return (
-		<ErrorBoundary
-			onError={(e) => addMessage({ message: e.message })}
-			fallback={<div>{latestMessage?.message}</div>}
-		>
-			{latestMessage && (
-				<Banner onClick={confirmLatestMessage}>
-					<h2
-						style={{
-							display: 'grid',
-							gap: '1rem',
-							alignItems: 'center',
-							gridTemplateColumns: '1fr 10fr 1fr',
-							padding: '0 2rem',
-						}}
-					>
-						{latestMessage.icon ? (
-							<span>{latestMessage.icon}</span>
-						) : (
-							<span></span>
-						)}
-						<span style={{ textAlign: 'center' }}>
-							{latestMessage?.message}
-						</span>{' '}
-						{!latestMessage.needsNoConfirmation && (
-							<FaRegCircleCheck size={battleSpriteSize} />
-						)}
-					</h2>
-				</Banner>
-			)}
-			<BaseSizeProvider allowedBaseSizes={allowedBaseSizes}>
-				<SaveFileProvider>
-					<LocationProvider>
-						<ScreenTransitionProvider>
-							<Router />
-						</ScreenTransitionProvider>
-					</LocationProvider>
-				</SaveFileProvider>
-			</BaseSizeProvider>
-		</ErrorBoundary>
-	);
+  return (
+    <ErrorBoundary
+      onError={(e) => addMessage({ message: e.message })}
+      fallback={<div>{latestMessage?.message}</div>}
+    >
+      {latestMessage && (
+        <MessageBanner
+          latestMessage={latestMessage}
+          confirmLatestMessage={confirmLatestMessage}
+        />
+      )}
+      <BaseSizeProvider allowedBaseSizes={allowedBaseSizes}>
+        <SaveFileProvider>
+          <LocationProvider>
+            <ScreenTransitionProvider>
+              <Router />
+            </ScreenTransitionProvider>
+          </LocationProvider>
+        </SaveFileProvider>
+      </BaseSizeProvider>
+    </ErrorBoundary>
+  );
+};
+
+const MessageBanner = ({
+  latestMessage,
+  confirmLatestMessage,
+}: {
+  latestMessage: Message;
+  confirmLatestMessage: () => void;
+}) => {
+  return (
+    <Banner onClick={confirmLatestMessage}>
+      <h2
+        style={{
+          display: "grid",
+          gap: "1rem",
+          alignItems: "center",
+          gridTemplateColumns: "1fr 10fr 1fr",
+          padding: "0 2rem",
+        }}
+      >
+        {latestMessage.icon ? <span>{latestMessage.icon}</span> : <span></span>}
+        <span style={{ textAlign: "center" }}>
+          {latestMessage?.message}
+        </span>{" "}
+        {!latestMessage.needsNoConfirmation && (
+          <FaRegCircleCheck size={battleSpriteSize} />
+        )}
+      </h2>
+    </Banner>
+  );
 };
