bdb22676c5ad6f5cc6612d84d58655775488296f
Chris Kuhtz <christopherkuhtz@gmail.com>
2026-05-01 07:34:59 +0200

    mini fixes

--- src/functions/getNextFieldOccupant.ts
+++ src/functions/getNextFieldOccupant.ts
@@ -1,10 +1,13 @@
-import { Occupant } from '../interfaces/Occupant';
+import { Occupant } from "../interfaces/Occupant";
 
 export const getNextFieldOccupant = (
-	focusedField: { x: number; y: number },
-	statefulOccupantsRecord: Occupant[]
+  focusedField: { x: number; y: number },
+  statefulOccupantsRecord: Occupant[],
 ): Occupant | undefined => {
-	return statefulOccupantsRecord.find(
-		(occ) => occ.x === focusedField.x && occ.y === focusedField.y
-	);
+  return statefulOccupantsRecord
+    .filter((occ) => occ.x === focusedField.x && occ.y === focusedField.y)
+    .sort((a) => {
+      return a.type === "BERRY_TREE" ? 1 : -1;
+    })
+    .at(0);
 };
